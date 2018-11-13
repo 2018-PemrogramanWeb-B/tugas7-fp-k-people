b1e37d2fda0e89b0353b33a4b4462ab6bbda465b
Atika Rizki <37141007+atikakiki@users.noreply.github.com>
2018-11-13 11:44:46 +0700

    Add files via upload

--- form.php
+++ form.php
@@ -26,7 +26,7 @@ function test_input($data) {
 
 <h2>Form Pendaftaran</h2>
 <div class="login-box">
-<form method="post" action="index2.html">
+<form method="post" action="daftar_berhasil.php">
   <p>Username: <input type="text" name="username">
   <br></p>
   <p>E-mail: <input type="text" name="email">
