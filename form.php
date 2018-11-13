d500c59076b928366009ef33ba9de4cd7fd59572
Atika Rizki <37141007+atikakiki@users.noreply.github.com>
2018-11-13 09:36:34 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<title>Sign Up</title>
+<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>  
+
+<?php
+
+$nama = $email = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nama = test_input($_POST["nama"]);
+  $email = test_input($_POST["email"]);
+  $password = test_input($_POST["password"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>Form Pendaftaran</h2>
+<div class="login-box">
+<form method="post" action="daftar_berhasil.php">
+  <p>Username: <input type="text" name="username">
+  <br></p>
+  <p>E-mail: <input type="text" name="email">
+  <br></p>
+  <p>Password: <input type="password" name="password">
+  <br></p>
+  <p>Konfirmasi Password: <input type="password" name="password">
+  <br></p>
+  <br>
+  <input type="submit" name="submit" value="Sign Up">
+</form>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+<div class="login-box">
+<img src="http://icons-for-free.com/free-icons/png/512/403023.png" class="avatar">
+<h1>Login Here</h1>
+<form method ="post" action="index.html">
+<p>Username:</p>
+<input type="text" name="username" placeholder="Enter Username"><br/>
+<p>Password:</p>
+<input type="password" name="password" placeholder="Enter Password"><br/>
+<input type="submit" name="submit" value="Login">
+<br>Belum Daftar?<a href="form.php">Daftar Disini</a>
+</form>
+</div>
+</body>
+
+</html>
