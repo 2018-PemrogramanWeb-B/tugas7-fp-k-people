635576006fceb4193012bddc232b3764e8488483
Atika Rizki <37141007+atikakiki@users.noreply.github.com>
2018-12-19 12:00:59 +0700

    Update indexkuy.php

--- indexkuy.php
+++ indexkuy.php
@@ -88,7 +88,7 @@ if(!isset($_SESSION['username'])) {
         
         <div class="row">
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1komunitas">
+            <a class="portfolio-link" data-toggle="modal" href="#popup">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -99,10 +99,32 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>ADDHAMEDA NATION</h4>
               <p class="text-muted">Dance Cover</p>
+    <div id= "popup">
+    	<div class="window">
+        	<a href="indexkuy.php #komunitas" class="close-button" title="Close">X</a>
+            <h2>ADDHAMEDA NATION</h2>
+            <img class="img-icon" src="img/portfolio/index.png">   
+            <h6>addhameda</h6>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>addhameda_nation</h6>
+            <img class="img-icon" src="img/portfolio/index5.png">   
+            <h6>addhameda nation</h6>
+            <div class ="text-info">
+            <h6>Domisili : Ngagel, Surabaya</h6>
+            <h6>Website : https://addhamedanation.wordpress.com/</h6>
+            <h6>Twitter : @addhamedaent</h6>
+            <h6>YouTube : ADDHAMEDA NATION</h6>
+            <h6>Deskripsi :
+            <br>Salah satu komunitas dance cover di Surabaya yang berfokus pada video project di youtube.
+             Jika ingin menjadi member dari komunitas ini, silahkan hubungi cp yang tertera di atas ya.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
+
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2komunitas">
+            <a class="portfolio-link" data-toggle="modal" href="#popup2">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -113,10 +135,23 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>STRAY KIDS SURABAYA</h4>
               <p class="text-muted">Fandom</p>
+              <div id="popup2">
+    	<div class="window">
+        	<a href="indexkuy.php #komunitas" class="close-button" title="Close">X</a>
+            <h2>STRAY KIDS SURABAYA</h2>
+            <div class ="text-info">
+            <h6>Domisili : Surabaya</h6>
+            <h6>Deskripsi :
+            <br>Komunitas fans dari 9 cowok ganteng yang tergabung dalam boygrup Stray Kids.
+          Jika ingin bergabung, bisa banget nih langsung chat ke line @indri_ku.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
+
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3komunitas">
+            <a class="portfolio-link" data-toggle="modal"  href="#popup3">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -127,10 +162,29 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>IFLS CULTURE</h4>
               <p class="text-muted">Management</p>
+              <div id= "popup3">
+    	<div class="window">
+        	<a href="indexkuy.php #komunitas" class="close-button" title="Close">X</a>
+            <h2>IFLS CULTURE</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>iflsculture</h6>
+            <div class ="text-info">
+            <h6>Domisili : Gedung SCC kampus ITS Sukolilo, Surabaya</h6>
+            <h6>YouTube : IFLS CULTURE</h6>
+            <h6>Deskripsi :
+            <br>Unit Kegiatan Mahasiswa kampus Institut Teknologi Sepuluh Nopember Surabaya
+          yang berfokus di bidang culture dari korea dan jepang nih. Ada dance cover, sing cover, yosakoi, band,
+        dan tari tradisional korea. Cocok banget nih buat mahasiswa ITS yang suka dengan culture dari kedua negara tersebut.
+      Komunitas ini ditujukan untuk mahasiswa ITS aja ya. Buat kalian yang mau gabung bisa langsung ke sekretariat IFLS di gedung SCC lantai 3
+    atau melalui OA UKM IFLS.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
+
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4komunitas">
+            <a class="portfolio-link" data-toggle="modal" href="#popup4">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -141,10 +195,27 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>IXORA DC</h4>
               <p class="text-muted">Dance Cover</p>
+           
+              <div id= "popup4">
+    	<div class="window">
+        	<a href="indexkuy.php #komunitas" class="close-button" title="Close">X</a>
+            <h2>IXORA DANCE COVER</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@ixora.dc</h6>
+            <div class ="text-info">
+            <h6>Domisili : Semarang</h6>
+            <h6>YouTube : IXORA DC</h6>
+            <h6>Deskripsi :
+            <br>Salah satu komunitas dance cover yang ada di Semarang. Ixora debut bulan februari 2018.
+          komunitas ini beranggotakan cewek-cewek cantik nih. Buat yang kepo atau mau gabung, bisa banget nih kunnjungi instagram dan youtube nya.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
+
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5komunitas">
+            <a class="portfolio-link" data-toggle="modal" href="#popup5">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -155,10 +226,27 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>VALKYRIE DC</h4>
               <p class="text-muted">Dance Cover</p>
+            
+              <div id= "popup5">
+    	<div class="window">
+        	<a href="indexkuy.php #komunitas" class="close-button" title="Close">X</a>
+            <h2>VALKYRIE DANCE COVER</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@Valkyrie_dc</h6>
+            <div class ="text-info">
+            <h6>Domisili : Surabaya</h6>
+            <h6>YouTube : Valkyrie DC</h6>
+            <h6>Deskripsi :
+            <br>Komunitas dance cover yang ada di Surabaya. Valkyrie ini sering banget menjuarai kompetisi
+        dance cover yang ada di Surabaya maupun luar Surabaya lho, bahkan sampai ke Jakarta. Buat yang kepo, bisa banget stalk instagram dan youtube nya.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
+          
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6komunitas">
+            <a class="portfolio-link" data-toggle="modal" href="#popup6">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -169,6 +257,24 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>FLAWLESS DANCE ENTERTAIMENT</h4>
               <p class="text-muted">Management</p>
+              <div id= "popup6">
+    	<div class="window">
+        	<a href="indexkuy.php #komunitas" class="close-button" title="Close">X</a>
+            <h2>FLAWLESS DANCE ENTERTAIMENT</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@fldcent_id</h6>
+            <div class ="text-info">
+            <h6>Domisili : RPTRA Jl. Masjid Al-muawanah RT.008/RW.007, srengseng sawah, jagakarsa, jaksel</h6>
+            <h6>YouTube : FLAWLESS DANCE ENTERTAIMENT</h6>
+            <h6>Deskripsi :
+            <br>Komunitas ini merupakan bagian dari Agency/Dance Entertainment, 
+            berpusat di Srengseng Sawah Jagakarsa Jaksel, 
+            komunitas ini bertujuan untuk menampung dan mengembangkan hobby dan bakat para pecinta kpop yang bisa kpop dance dan suka menyanyi lagu kpop. 
+            Latihan di RPTRA Al-Muawanah srengseng sawah, setiap hari minggu dari jam 11 siang - selesai.
+            Tinggal hubungi id line : putri_kyeran, setelah itu datang ke tempat latihan, evaluasi, satu minggu kemudian audisi </h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
         </div>
@@ -189,7 +295,7 @@ if(!isset($_SESSION['username'])) {
         </div>
         <div class="row">
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <a class="portfolio-link" data-toggle="modal" href="#popup7">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -200,10 +306,30 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>KCF AWARDS 2018</h4>
               <p class="text-muted">Competition</p>
+    <div id="popup7">
+    <div class="window">
+        	<a href="indexkuy.php #kevent" class="close-button" title="Close">X</a>
+            <h2>Korean Culture Festival AWARDS</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@sabana.ent</h6>
+            <img class="img-icon" src="img/portfolio/index5.png">   
+            <h6>@sabana.ent</h6>
+            <div class ="text-info">
+            <h6>Tempat : L1 BG Junction Surabaya</h6>
+            <h6> Waktu : 30 Desember 2018, 12:00 AM - till drop</h6>
+            <h6> Cp : 083856445151 (WA)</h6>
+            <h6>Deskripsi :
+            <br>Ajang awarding dari KCF session 1 sampai 12. KCF sendiri merupakan kompetisi dance dan sing cover
+          yang diadakan oleh Sabana entertaiment setiap bulanya. Pemenang setiap session nya akan mendapatkan 
+        nominasi di KCF Award akhir tahun.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
+          
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+            <a class="portfolio-link" data-toggle="modal" href="#popup8">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -212,12 +338,35 @@ if(!isset($_SESSION['username'])) {
               <img class="img-fluid" src="img/portfolio/09-thumbnail.jpg" alt="">
             </a>
             <div class="portfolio-caption">
-              <h4>INEFFABLE MOMENT</h4>
-              <p class="text-muted">Gathering</p>
+              <h4>PAJAMA'S PARTY with PINK PANDA</h4>
+              <p class="text-muted">Gathering and competition</p>
+              <div id="popup8">
+    <div class="window">
+        	<a href="indexkuy.php #kevent" class="close-button" title="Close">X</a>
+            <h2>PAJAMA'S PARTY with PINK PANDA</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@nightkidsent</h6>
+            <img class="img-icon" src="img/portfolio/index5.png">   
+            <h6>@nightkidsent</h6>
+            <div class ="text-info">
+            <h5>Exclusive Pajamas Party
+            <h6>Saturday, 26th January 2019, 18.00 - 20.00 WIB
+            <h6>Midtown Residence Surabaya
+            <h5>Kimchi-Go's Party Dance Cover Competition
+            <h6>Sunday, 27th January 2019, 13.00 WIB - End
+            <h6>Marvell City Mall Surabaya  
+            <h6> Cp : @CJG6600G (Line)</h6>
+            <h6>Deskripsi :
+            <br>Night Kids Entertaiment bekerjasama dengan Kimchi.go mengajak kpopers Surabaya untuk seru-seruan 
+          bareng PINK PANDA. Selain pajama party, ada juga 
+        sing cover dan dance cover competition. More info bisa cek di sosmed nya Nightkids entertaiment ya.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+            <a class="portfolio-link" data-toggle="modal" href="#popup9">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -225,13 +374,36 @@ if(!isset($_SESSION['username'])) {
               </div>
               <img class="img-fluid" src="img/portfolio/10-thumbnail.jpg" alt="">
             </a>
-            <div class="portfolio-caption">
-              <h4>KOREAN DANCE CHAMPIONSHIP ROOKIE WAR</h4>
-              <p class="text-muted">Competition</p>
+            <div class="portfolio-caption"> 
+              <h4>ARMY SURABAYA 4th ANNIVERSARY</h4>
+              <p class="text-muted">Gathering, Birthday Party, Exhibition</p>
+              <div id="popup9">
+    <div class="window">
+        	<a href="indexkuy.php #kevent" class="close-button" title="Close">X</a>
+            <h2>ARMY SURABAYA 4th ANNIVERSARY</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@bts_surabaya</h6>
+            <div class ="text-info">
+            <h6>FEBRUARY 10TH 2019 AT MULTIFUNCTION HALL STAR 1-2 BG JUNCTION SURABAYA OPEN GATE: START 11 AM - 2 PM  
+            <h6> Cp :  NIKKI (chat only) LINE: NIKKIPARAMYTA WA: 0822 3104 1968 </h6>
+            <h6>Deskripsi :
+            <br>ARMY! ARE YOU READY?? . . . WILL BE HELD, THE BIGGEST EVENT OF ARMY SURABAYA 
+            <br>▶️ 4TH ANNIVERSARY OF ARMY SURABAYA 
+            <br>▶️ SOPE BIRTHDAY PARTY 
+            <br>▶️ BTS EXHIBITION (SUMMER RAIN IN OUR GALAXY) 
+            <br>PERFORMANCE BY 
+            <br>✅ HELL MARCO 
+            <br>✅ AERION 
+            <br>✅ APB 
+            <br>✅ BTW PRESALE 1
+            <br>IDR 100.000 (until December 31th 2018)</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+            <a class="portfolio-link" data-toggle="modal" href="#popup11">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -242,10 +414,26 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>DUO KIM DAY</h4>
               <p class="text-muted">Gathering</p>
+              <div id="popup11">
+    <div class="window">
+        	<a href="indexkuy.php #kevent" class="close-button" title="Close">X</a>
+            <h2>DUO KIM DAY</h2>           
+            <div class ="text-info">
+            <h6>🎉🎉🎉 Halo! Taretan-taretan ARMY, kami hadir untuk merayakan bersama ulang tahun DUO WORLDWIDE HANDSOME, Mr. Kim Seokjin dan Mr. Kim Taehyung.
+            <h6>Ikuti dan meriahkan!!! Akan ada banyak hadiah dan keseruan lainnya.  
+            <h6>📍 Bro N Sis Cafe [Sekitar TK Siti Khodijah].
+            <h6>📅 Minggu, 23 Desember 2018
+            <h6>🕙 10:00 - selesai
+            <h6>🎫 50K
+            <h6>🤸🏻‍♂️ Special Performance by ... Informasi lebih lanjut hubungi, • Eka :+6285546495427
+            <h6>• Tutut : +6281231235665 [With fanbase partner, BTS X ARMY JATIM]. Sponsored by Najikasora Online Shop 
+</div>
+</div>
+          </div>
             </div>
           </div>
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+            <a class="portfolio-link" data-toggle="modal" href="#popup11">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -256,10 +444,30 @@ if(!isset($_SESSION['username'])) {
             <div class="portfolio-caption">
               <h4>THE K-COLLOSAL</h4>
               <p class="text-muted">Competition</p>
+              <div id="popup11">
+    <div class="window">
+        	<a href="indexkuy.php #kevent" class="close-button" title="Close">X</a>
+            <h2>THE K-COLLOSAL</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@thek.collosal</h6>            
+            <div class ="text-info">
+            <h6>Sunday, 20th January 2019
+            <h6>Multifunction Hall BG Junction, SURABAYA at 11.00 until end.
+            <h6>Dinner with GS : Saturday, 19th January 2019
+            <h6>Guest Star : @sinister_id
+            <h6>Cover Dance and sing cover Competition.
+            <h6>COMPETITION REGISTRATION.
+            <h6>CP : 08970691813 (wa)
+            <h6>Tiket Counter : 082124717226 (wa only) or visit CHOKIWA KSHOP Royal Plaza UG.floor, E5-27 (Gramedia) 
+<h6>UNNICORN.STATION
+BG Junction L1. floor, B65 (belakang hartono)</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
           <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+            <a class="portfolio-link" data-toggle="modal" href="#popup12">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fas fa-plus fa-3x"></i>
@@ -268,8 +476,27 @@ if(!isset($_SESSION['username'])) {
               <img class="img-fluid" src="img/portfolio/13-thumbnail.jpg" alt="">
             </a>
             <div class="portfolio-caption">
-              <h4>4TH GATHERING NCTZEN MALANG</h4>
-              <p class="text-muted">Gathering</p>
+              <h4>NOBAR ONE : THE WORLD IN SEOUL</h4>
+              <p class="text-muted">Gathering, Nonton Bareng</p>
+              <div id="popup12">
+    <div class="window">
+        	<a href="indexkuy.php #kevent" class="close-button" title="Close">X</a>
+            <h2>Nonton Bareng DVD ONE:THE WORLD IN SEOUL</h2>
+            <img class="img-icon" src="img/portfolio/index3.jpg">   
+            <h6>@kpopcafesby</h6>            
+            <div class ="text-info">
+            <h6>SAUDADE ㅡ a memory we won't to forget.
+            <h6>Wanna One World Tour “ONE : THE WORLD” DVD screening in Surabaya.
+            <h6>Hari & Tanggal : Minggu, 20 Januari 2019
+            <h6>Jam : 09:00 WIB - selesai.
+            <h6>HTM : IDR 75.000
+            <h6>Untuk pendaftaran silahkan isi form dibawah ini:
+              <h6>https://goo.gl/forms/DZS4lipNsktE3q4b2
+
+            <h6>Jika ada pertanyaan atau kendala lain, mohon segera hubungi admin twt & ig @kpopcafesby. ** We will update soon about detail goodies and doorprizes.</h6> 
+</div>      
+          </div>
+    </div>
             </div>
           </div>
         </div>
